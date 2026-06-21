7988c4121b6942ff7c1b21bd301a06b3a36de343
Jannael <orlandojannael@gmail.com>
2026-06-21 11:38:50 -0600

    chore: remove raw schema.sql in favor of Drizzle

--- apps/api/data/schema.sql
+++
@@ -1,8 +0,0 @@
--- only name and account for now, we can add more fields later if needed
-
-CREATE TABLE IF NOT EXISTS inventory_elements (
-    id TEXT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    enable BOOLEAN NOT NULL,
-    account VARCHAR(255) NOT NULL,
-);
